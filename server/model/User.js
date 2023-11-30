6a41e7a254357e289f3abb970ad0067776c75f5e
aziz-codes <azizisoffline@gmail.com>
2023-11-30 23:35:32 +0500

    enabled mongoose timestamp

--- server/model/User.js
+++ server/model/User.js
@@ -1,25 +1,28 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    unique,
-    required,
-  },
-  username: {
-    type: String,
-    required,
-    unique,
-  },
-  password: {
-    type: String,
-    required,
-  },
-});
+  { timestamps: true }
+);
 const User = mongoose.model("users", userSchema);
 
 export default User;
